b03665c6a6748f91b7cc0f09cd476e31c6bcac7d
Lakshmana Chowdari <lakshmanachowdarin@gmail.com>
2026-03-10 20:55:40 +0530

    QSMS-3: Actuator endpoints defined.

--- question-service/src/main/java/com/lakshman/question_service/Service/CategoryService.java
+++ question-service/src/main/java/com/lakshman/question_service/Service/CategoryService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
-import java.util.ArrayList;
 
 @Service
 public class CategoryService {
